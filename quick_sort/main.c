a49c538ce6305355f80657930bddb6b49d49c7f9
Grisha <g.meshchaninov@g.nsu.ru>
2020-11-02 16:16:49 +0700

    ready

--- quick_sort/main.c
+++ quick_sort/main.c
@@ -6,113 +6,69 @@ int print_arr(double arr[], int start, int end){
         printf("%lf ", arr[i]);
         i++;
     }
+    printf("\n");
     return 0;
 }
 
-int quick_sort_recurs(double arr[], int n, int smaller, int bigger){
-    if (n > 1){
-        int centre = (smaller + bigger) / 2;
-        double element = arr[centre];
+int quick_sort_recurs(double arr[], int smaller, int bigger) {
+    if (bigger - smaller > 1) {
         double swap;
+        double centre = arr[(smaller + bigger + 1) / 2];
         int start = smaller;
         int end = bigger;
-        print_arr(arr, 0, 7);
-        printf(" all\n");
-        print_arr(arr, start, end + 1);//--------------------------------------------------------
-        printf(" 0\n");
-        while (smaller != centre && bigger != centre){
-            if (arr[smaller] >= element){
-                while (bigger != centre){
-                    if (arr[bigger] < element){
+        while (smaller < bigger) {
+            int stop = 0;// if (stop = 1) { break the cycle while (smaller < bigger)}
+            if (arr[smaller] >= centre && arr[bigger] > centre) {//-------------------------------------
+                while (arr[bigger] > centre) {
+                    bigger--;
+                    if (smaller >= bigger) {
+                        stop = 1;
                         break;
                     }
-                    bigger--;
                 }
-                if (bigger == centre){
-                    break;
+            } else if (arr[smaller] < centre && arr[bigger] < centre) {
+                while (arr[smaller] < centre) {
+                    smaller++;
+                    if (smaller >= bigger) {
+                        stop = 1;
+                        break;
+                    }
                 }
-                swap = arr[smaller];
-                arr[smaller]= arr[bigger];
-                arr[bigger] = swap;
+            } else if (arr[smaller] < centre && arr[bigger] >= centre){
+                smaller++;
                 bigger--;
+                continue;
             }
+            if (stop == 1){
+                break;
+            }
+            swap = arr[smaller];
+            arr[smaller] = arr[bigger];
+            arr[bigger] = swap;
             smaller++;
+            bigger--;
+            if (smaller > bigger){
+                smaller--;
+                bigger++;
+                break;
+            }
         }
-        print_arr(arr, 0, 7);
-        printf(" all\n");
-        print_arr(arr, start, end + 1);//--------------------------------------------------------
-        printf(" 1\n");
-        if (smaller == centre && bigger == centre){
-            quick_sort_recurs(arr, (centre - 1) - start, start, centre - 1);
-            printf("1.1\n");
-            quick_sort_recurs(arr, end - (centre - 1), centre + 1, end);
-            printf("1.5,\n");
-            return 0;
-        }else if (smaller == centre && bigger != centre){
-            int prosto = centre + 1;
-            while (prosto != bigger){
-                if (arr[prosto] >= element){
-                    prosto++;
-                }else{
-                    int diff = prosto - centre;
-                    swap = arr[prosto];
-                    arr[prosto]= arr[centre];
-                    arr[centre] = swap;
-                    for (int i=0; i<diff; i++){
-                        swap = arr[prosto - i];
-                        arr[prosto - i]= arr[prosto - i - 1];
-                        arr[prosto - i - 1] = swap;
-                    }
-                    centre++;
-                    prosto = centre + 1;
-                }
-                print_arr(arr, 0, 7);
-                printf(" all\n");
-                print_arr(arr, start, end + 1);//----------------------------------------------------
-                printf(" 2\n");
-            }
-            quick_sort_recurs(arr, (centre - 1) - start, start, centre - 1);
-            printf("asdfgh\n");
-            quick_sort_recurs(arr, end - (centre - 1), centre + 1, end);
-            printf("okijl,\n");
-            return 0;
-        }else if (smaller != centre && bigger == centre){
-            int prosto = centre - 1;
-            while (prosto != smaller){
-                if (arr[prosto] < element){
-                    prosto--;
-                }else{
-                    int diff = centre - prosto;
-                    swap = arr[prosto];
-                    arr[prosto]= arr[centre];
-                    arr[centre] = swap;
-                    for (int i=0; i<diff; i++){
-                        swap = arr[prosto + i];
-                        arr[prosto + i]= arr[prosto + i + 1];
-                        arr[prosto + i + 1] = swap;
-                    }
-                    centre--;
-                    prosto = centre - 1;
-                }
-                print_arr(arr, 0, 7);
-                printf(" all\n");
-                print_arr(arr, start, end + 1);//------------------------------------------------
-                printf(" 3\n");
-            }
-            quick_sort_recurs(arr, (centre - 1) - start, start, centre - 1);
-            printf("asdfgh\n");
-            quick_sort_recurs(arr, end - (centre - 1), centre + 1, end);
-            printf("okijl,\n");
-            return 0;
+        quick_sort_recurs(arr, start, smaller);
+        quick_sort_recurs(arr, bigger, end);
+    } else if (bigger - smaller == 1){
+        if (arr[smaller] > arr[bigger]){
+            double swap;
+            swap = arr[smaller];
+            arr[smaller] = arr[bigger];
+            arr[bigger] = swap;
         }
     }
-    printf("qwerty\n");
     return 0;
 }
 
 int quick_sort(double arr[], int n){
     int small = 0, big = n - 1;
-    quick_sort_recurs(arr, n, small, big);
+    quick_sort_recurs(arr, small, big);
     return 0;
 }
 
